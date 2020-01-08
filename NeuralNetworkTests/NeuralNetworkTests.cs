6c298d62c02e56219c20771ce7a18d32b15c8e08
Дмитрий <dima-tkachenko@mail.ru>
2020-01-09 00:29:09 +0300

    Реализовано преобразование изображения в черно-белый формат

--- NeuralNetworkTests/NeuralNetworkTests.cs
+++ NeuralNetworkTests/NeuralNetworkTests.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Linq;
 
 namespace NeuralNetwork.Tests
 {
@@ -22,6 +23,7 @@ namespace NeuralNetwork.Tests
                 // Курит S
                 // Правильно питается F
                 //T  A  S  F
+                { 0, 0, 0, 0 },
                 { 0, 0, 0, 1 },
                 { 0, 0, 1, 0 },
                 { 0, 0, 1, 1 },
@@ -47,6 +49,48 @@ namespace NeuralNetwork.Tests
             {
                 var row = NeuralNetwork.GetRow(inputs, i);
                 var res = neuralNetwork.FeedForward(row).Output;
+                results.Add(res);
+            }
+            for (int i = 0; i < results.Count; i++)
+            {
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+        [TestMethod()]
+        public void DatasetTest()
+        {
+            var outputs = new List<double>();
+            var inputs = new List<double[]>();
+            using (var sr = new StreamReader("heart.csv"))
+            {
+                var header = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var row = sr.ReadLine();
+                    var values = row.Split(',').Select(v => Convert.ToDouble(v.Replace(".", ","))).ToList();
+                    var output = values.Last();
+                    var input = values.Take(values.Count - 1).ToArray();
+                    outputs.Add(output);
+                    inputs.Add(input);
+                }
+            }
+            var inputSignals = new double[inputs.Count, inputs[0].Length];
+            for (int i = 0; i < inputSignals.GetLength(0); i++)
+            {
+                for (var j = 0; j < inputSignals.GetLength(1); j++)
+                {
+                    inputSignals[i, j] = inputs[i][j];
+                }
+            }
+            var topology = new Topology(outputs.Count, 1, 0.1, outputs.Count / 2);
+            var neuralNetwork = new NeuralNetwork(topology);
+            var difference = neuralNetwork.Learn(outputs.ToArray(), inputSignals, 1000);
+            var results = new List<double>();
+            for (int i = 0; i < outputs.Count; i++)
+            {
+                var res = neuralNetwork.FeedForward(inputs[i]).Output;
                 results.Add(res);
             }
             for (int i = 0; i < results.Count; i++)
